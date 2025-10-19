f0aa7b09fdc772a69294df593f75b5c9ec1b8034
davidgma <dmlh02@gmail.com>
2025-10-19 21:40:26 +0100

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { SlItems } from './views/sl-items/sl-items.js';
 import { SlvSheet } from './views/slv-sheet/slv-sheet.js';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: SlItems },
     { path: 'settings', component: SlvSettings },
     { path: 'sheet', component: SlvSheet },
