b137aa2ad94aaaa505781e1ffa6478e7cbb1c545
davidgma <dmlh02@gmail.com>
2025-10-19 21:12:48 +0100

    a

--- src/app/views/slv-settings/slv-settings.ts
+++ src/app/views/slv-settings/slv-settings.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class SlvSettings {
 
+  private theDefault = {
+    spreadsheetId: "1YZWmLktxzprYWLZDHopC0vsz_Z44eavyHyo0lgWsSj4",
+    clientId: "285838277656-epg5b87qpis468k0r6crifeiq8m68djf.apps.googleusercontent.com",
+    APIkey: "AIzaSyBYAKEDgQCyvtwbUk0Gws7obBycBCva99E",
+    listRange: "list",
+    cookiesRange: "cookies",
+    authenticationToken: "",
+    accessToken: "",
+    accessTokenExpiry: null, // number of seconds since 1970, when token expires
+    version: 1,
+    listData: "",
+    cookiesData: ""
+  }
 }
