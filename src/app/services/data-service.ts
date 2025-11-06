cd5c797fbc49bf914f2361f6282c4fbfc961a0fe
davidgma <dmlh02@gmail.com>
2025-11-06 19:01:18 +0000

    a

--- src/app/services/data-service.ts
+++ src/app/services/data-service.ts
@@ -4,19 +4,67 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
-  public spreadsheetId = "initial";
+  public spreadsheetId = "initial2";
+  public debug: string;
+  private defaults: Defaults;
 
-  private theDefault = {
-    spreadsheetId: "1YZWmLktxzprYWLZDHopC0vsz_Z44eavyHyo0lgWsSj4",
-    clientId: "285838277656-epg5b87qpis468k0r6crifeiq8m68djf.apps.googleusercontent.com",
-    APIkey: "AIzaSyBYAKEDgQCyvtwbUk0Gws7obBycBCva99E",
-    listRange: "list",
-    cookiesRange: "cookies",
-    authenticationToken: "",
-    accessToken: "",
-    accessTokenExpiry: null, // number of seconds since 1970, when token expires
-    version: 1,
-    listData: "",
-    cookiesData: ""
+  constructor() {
+
+    this.defaults = new Defaults(
+      "1YZWmLktxzprYWLZDHopC0vsz_Z44eavyHyo0lgWsSj4", //spreadsheetId
+      "285838277656-epg5b87qpis468k0r6crifeiq8m68djf.apps.googleusercontent.com", //cliendId
+      "AIzaSyBYAKEDgQCyvtwbUk0Gws7obBycBCva99E", //APIkey
+      "list", //listRange
+      "cookies", //cookiesRange
+      "", //authenticationToken
+      null, // accessTokenExpiry number of seconds since 1970, when token expires
+      1, //version
+      "", //listData
+      ""); //cookiesData
+  
+   this.debug = this.defaults.toString();
+
+    const spreadsheetIdStored = localStorage.getItem("spreadsheetId");
+    if (spreadsheetIdStored === null) {
+      // this.debug = "spreadsheetId is null ";
+      localStorage.setItem("spreadsheetId", this.spreadsheetId);
+    }
+    else {
+      // this.debug = "spreadsheetId: " + spreadsheetIdStored;
+      this.spreadsheetId = spreadsheetIdStored;
+    }
+
+
+  }
+
+  
+}
+
+class Defaults {
+  constructor(
+    private _spreadsheetId: string,
+    private _cliendId: string,
+    private _APIkey: string,
+    private _listRange: string,
+    private _cookiesRange: string,
+    private _authenticationToken: string,
+    private _accessTokenExpiry: number | null,
+    private _version: number,
+    private _listData: string,
+    private _cookiesData: string
+  ) { }
+
+  toString(): string {
+    return "\n" + "spreadsheetId: " + this._spreadsheetId + "\n"
+    + "cliendId: " + this._cliendId + "\n"
+    + "APIkey: " + this._APIkey + "\n"
+    + "cliendId: " + this._cliendId + "\n"
+    + "cliendId: " + this._cliendId + "\n"
+    + "cliendId: " + this._cliendId + "\n"
+    + "cliendId: " + this._cliendId + "\n"
+    + "cliendId: " + this._cliendId + "\n"
+    + "cliendId: " + this._cliendId + "\n"
+    + "cliendId: " + this._cliendId + "\n"
+    ;
   }
 }
